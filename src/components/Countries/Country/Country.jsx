5bff4510e3567c5d6e56222fbd2439bde728b4ec
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-03-23 20:10:48 +0600

    add countrys photo and money symbol

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Country = ({country}) => {
-    console.log(country)
+const Country = ({ country }) => {
+    console.log()
+    const [name, setName] = useState(false)
     return (
         <div className='card shadow m-2 p-3'>
-            <h1>{country.name.common}</h1>
+            <img src={country.flags.flags.png} alt="" />
+            <h1>{name ? country.name.official : country.name.common} </h1>
+            <h1>{country.ccn3.ccn3}</h1>
+            <h1>{country.cca3.cca3}</h1>
+            <h1>{}</h1>
+
+            {
+                Object.values(country.currencies.currencies).map(data=><p>{data.symbol}  {data.name}</p>)
+            }
+
+            <button className='btn' onClick={() => setName(!name)}>{name ? "Common" : "Official"}</button>
+            
         </div>
     );
 };
